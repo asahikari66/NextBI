8d240a6e0d1d85956394cc08db1d3346132c6269
admin <1923138308@qq.com>
2026-05-21 15:12:32 +0800

    this is a framewor of server

---
+++ nextbi-server/src/test/java/com/nextbi/nextbi/NextbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextbi.nextbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextbiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
